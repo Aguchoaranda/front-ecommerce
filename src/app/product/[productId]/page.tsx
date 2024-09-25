1fae1dbfb2366c92b8b9518a3343c10f79728174
augusto <augustoaranda22@gmail.com>
2024-09-25 16:34:50 -0300

    fix deployment, disable some rules at .eslintrc.json

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,24 +1,12 @@
-"use client"
+// src/app/product/[productId]/page.tsx
 import { getProductsById } from "@/helpers/products.helpers";
 import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const Detail: React.FC<{ params: { productId: string } }> = ({ params }) => {
-  const [product, setProduct] = useState<any>(null); // Ajusta el tipo según tu estructura de datos
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const fetchedProduct = await getProductsById(params.productId);
-      setProduct(fetchedProduct);
-    };
-
-    fetchProduct();
-  }, [params.productId]);
-
-  if (!product) {
-    return <div>Cargando...</div>; // O cualquier indicador de carga
-  }
-
+// Define el componente como asincrónico
+const Detail = async ({ params }: { params: { productId: string } }) => {
+  const product = await getProductsById(params.productId);
+  
   return (
     <div>
       <ProductDetail {...product} />
@@ -26,4 +14,22 @@ const Detail: React.FC<{ params: { productId: string } }> = ({ params }) => {
   );
 };
 
+// No es necesario exportar como default, pero si lo haces, asegúrate de no tener conflictos en las rutas.
 export default Detail;
+
+
+// import { getProductsById } from "@/helpers/products.helpers";
+// import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
+// import React from "react";
+
+// export const Detail: React.FC <{params: {productId:string}}> = async ({params}) => {
+//   const product = await getProductsById(params.productId)
+//   return (
+//   <div> 
+//     <ProductDetail {...product}/>
+//   </div>)
+// };
+
+// export default Detail;
+
+
