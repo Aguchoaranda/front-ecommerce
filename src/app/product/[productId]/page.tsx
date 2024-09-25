edac8ee5f0fd1ce90f7538a7b7801066e3726a0d
augusto <augustoaranda22@gmail.com>
2024-09-25 16:45:11 -0300

    fix deployment, disable some rules at .eslintrc.json

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,35 +1,35 @@
-// src/app/product/[productId]/page.tsx
-import { getProductsById } from "@/helpers/products.helpers";
-import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
-import React from "react";
-
-// Define el componente como asincrónico
-const Detail = async ({ params }: { params: { productId: string } }) => {
-  const product = await getProductsById(params.productId);
-  
-  return (
-    <div>
-      <ProductDetail {...product} />
-    </div>
-  );
-};
-
-// No es necesario exportar como default, pero si lo haces, asegúrate de no tener conflictos en las rutas.
-export default Detail;
-
-
+// // src/app/product/[productId]/page.tsx
 // import { getProductsById } from "@/helpers/products.helpers";
 // import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
 // import React from "react";
 
-// export const Detail: React.FC <{params: {productId:string}}> = async ({params}) => {
-//   const product = await getProductsById(params.productId)
+// // Define el componente como asincrónico
+// const Detail = async ({ params }: { params: { productId: string } }) => {
+//   const product = await getProductsById(params.productId);
+  
 //   return (
-//   <div> 
-//     <ProductDetail {...product}/>
-//   </div>)
+//     <div>
+//       <ProductDetail {...product} />
+//     </div>
+//   );
 // };
 
+// // No es necesario exportar como default, pero si lo haces, asegúrate de no tener conflictos en las rutas.
 // export default Detail;
+
+
+import { getProductsById } from "@/helpers/products.helpers";
+import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
+import React from "react";
+
+export const Detail: React.FC <{params: {productId:string}}> = async ({params}) => {
+  const product = await getProductsById(params.productId)
+  return (
+  <div> 
+    <ProductDetail {...product}/>
+  </div>)
+};
+
+export default Detail;
 
 
