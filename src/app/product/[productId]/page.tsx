346563d62f6658720a912960adb5cd5d7dd106a4
augusto <augustoaranda22@gmail.com>
2024-09-25 16:58:10 -0300

    fix deployment, disable some rules at .eslintrc.json

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -3,18 +3,32 @@ import { getProductsById } from "@/helpers/products.helpers";
 import { ProductDetail } from "@/views/ProductDetail/ProductDetail";
 import React from "react";
 
-// Define el componente como asincrónico
+
 const Detail = async ({ params }: { params: { productId: string } }) => {
   const product = await getProductsById(params.productId);
-  
+
+  if (!product) {
+    return <div>Product not found</div>; // O maneja el caso adecuadamente
+  }
+
   return (
     <div>
       <ProductDetail {...product} />
     </div>
   );
 };
+// // Define el componente como asincrónico
+// const Detail = async ({ params }: { params: { productId: string } }) => {
+//   const product = await getProductsById(params.productId);
+  
+//   return (
+//     <div>
+//       <ProductDetail {...product} />
+//     </div>
+//   );
+// };
 
-// No es necesario exportar como default, pero si lo haces, asegúrate de no tener conflictos en las rutas.
+// // No es necesario exportar como default, pero si lo haces, asegúrate de no tener conflictos en las rutas.
 export default Detail;
 
 
@@ -30,6 +44,5 @@ export default Detail;
 //   </div>)
 // };
 
-// export default Detail;
-
+//  export default Detail;
 
